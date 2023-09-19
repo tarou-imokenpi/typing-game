3a50c521196da0513affc68aea14881c79431667
Tarou <91922753+tarou-imokenpi@users.noreply.github.com>
2023-09-20 08:52:36 +0900

    feat: タイプされた文字が表示されるようにした

--- web_ui/script.js
+++ web_ui/script.js
@@ -3,35 +3,34 @@ var targetText  = ["typing","tarou"];
 var current_text = 0; // 現在のテキストのインデックス
 var currentIndex = 0; // 現在の文字のインデックス
 
-var typing_text = document.getElementById("typing_text");
+var type_text = document.getElementById("type_text");
+var typed_text = document.getElementById("typed_text");
 
-typing_text.innerText = targetText[current_text];
+type_text.innerText = targetText[current_text];
 
 // EventListener
 document.addEventListener("keypress",(event) =>{
     type_key = event.key;
-
+    
 
     if (type_key == targetText[current_text][currentIndex] ){
         console.log(targetText[current_text][currentIndex]);
         console.log("一致");
+        typed_text.innerHTML = `<span style="background-color: yellow">${typed_text.textContent + type_key}</span>`
         currentIndex++;
     }
         if (currentIndex == targetText[current_text].length){
             console.log("問題クリア！！");
             currentIndex = 0; 
             current_text++;
+            typed_text.innerText = "";
             if (current_text < targetText.length){
-            typing_text.innerText = targetText[current_text];
+            type_text.innerText = targetText[current_text];
             }
             else{
-                typing_text.innerText = "ゲームクリア！！"
+                type_text.innerText = "ゲームクリア！！"
             }
 }
-        
-    else {
-        console.log("不一致");
-    }
 });
 
 
