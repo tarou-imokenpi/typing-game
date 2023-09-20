6b7131648e8eff15165885e9926bec8735dbfd9c
Tarou <91922753+tarou-imokenpi@users.noreply.github.com>
2023-09-21 08:55:15 +0900

    feat: 入力位置が分かるようにした

--- web_ui/script.js
+++ web_ui/script.js
@@ -6,8 +6,10 @@ var currentIndex = 0; // 現在の文字のインデックス
 // HTMLElementの取得
 var type_text = document.getElementById("type_text");
 var typed_text = document.getElementById("typed_text");
+var typing_text = document.getElementById("typing_text");
 
 type_text.innerText = targetText[current_text];
+typing_text.innerText = targetText[current_text];
 
 // EventListener
 document.addEventListener("keypress",(event) =>{
@@ -15,18 +17,17 @@ document.addEventListener("keypress",(event) =>{
     
 
     if (type_key == targetText[current_text][currentIndex] ){
-        console.log(targetText[current_text][currentIndex]);
-        console.log("一致");
-        typed_text.innerHTML = `<span style="background-color: yellow">${typed_text.textContent + type_key}</span>`
+        typing_text.innerText = typing_text.textContent.slice(1,typing_text.length)
+        typed_text.innerHTML = `<span style="background-color: yellow">${typed_text.textContent + type_key}</span>`;
         currentIndex++;
     }
         if (currentIndex == targetText[current_text].length){
-            console.log("問題クリア！！");
             currentIndex = 0; 
             current_text++;
             typed_text.innerText = "";
             if (current_text < targetText.length){
             type_text.innerText = targetText[current_text];
+            typing_text.innerText = targetText[current_text];
             }
             else{
                 type_text.innerText = "ゲームクリア！！"
